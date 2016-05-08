dd6c5c61b44e383fff2de33a139a0e631e267d97
ligorlwow <ligorlwow@gmail.com>
2016-05-08 23:57:17 +0300

    MovableListView solution with example.

---
+++ MovableListView/MovableListView/MovableListView.iOS/MovableCellGestureRecognizer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+namespace MovableListView.iOS
+{
+    public class MovableCellGestureRecognizer : UILongPressGestureRecognizer
+    {
+        private readonly WeakReference<ListView> weakListView;
+        private readonly WeakReference<UITableView> weakTableView;
+        private readonly WeakReference<UITableViewCell> weakNativeCell;
+        private readonly WeakReference<MovableViewCell> weakCell;
+        private NSIndexPath sourceOrNewAppliedIndexPath;
+        private UIView opaqueView;
+
+        private MovableCellGestureRecognizer(ListView listView, UITableView tableView, MovableViewCell cell, UITableViewCell nativeCell) : base(OnRecognizingAction)
+        {
+            weakListView = new WeakReference<ListView>(listView);
+            weakTableView = new WeakReference<UITableView>(tableView);
+            weakNativeCell = new WeakReference<UITableViewCell>(nativeCell);
+            weakCell = new WeakReference<MovableViewCell>(cell);
+        }
+
+        public static MovableCellGestureRecognizer CreateGesture(ListView listView, UITableView tableView, MovableViewCell cell, UITableViewCell nativeCell)
+        {
+            return new MovableCellGestureRecognizer(listView, tableView, cell, nativeCell);
+        }
+
+        private static void OnRecognizingAction(UILongPressGestureRecognizer r)
+        {
+            var recognizer = r as MovableCellGestureRecognizer;
+            if (recognizer == null)
+                throw new InvalidOperationException(string.Format("UILongPressGestureRecognizer must be MovableCellGestureRecognizer ({0})", r.GetType()));
+
+            recognizer.HandleRecognizerAction();
+        }
+
+        private void HandleRecognizerAction()
+        {
+            UITableView tableView;
+            weakTableView.TryGetTarget(out tableView);
+            UITableViewCell nativeCell;
+            weakNativeCell.TryGetTarget(out nativeCell);
+            ListView listView;
+            weakListView.TryGetTarget(out listView);
+            MovableViewCell cell;
+            weakCell.TryGetTarget(out cell);
+            if (tableView == null || nativeCell == null || listView == null || cell == null)
+                return;
+            var newPoint = LocationInView(tableView);
+            var newRowIndexPath = tableView.IndexPathForRowAtPoint(newPoint);
+            switch (State)
+            {
+                case UIGestureRecognizerState.Possible:
+                    break;
+                case UIGestureRecognizerState.Began:
+                    if (newRowIndexPath != null)
+                    {
+                        sourceOrNewAppliedIndexPath = newRowIndexPath;
+                        opaqueView = new UIView(new CGRect(new CGPoint(0, 0), nativeCell.Frame.Size)) { BackgroundColor = UIColor.Black.ColorWithAlpha(0.2f) };
+                        nativeCell.AddSubview(opaqueView);
+                    }
+                    break;
+                case UIGestureRecognizerState.Changed:
+                    if (sourceOrNewAppliedIndexPath == null || newRowIndexPath == null)
+                        break;
+
+                    if (!Equals(sourceOrNewAppliedIndexPath, newRowIndexPath))
+                    {
+                        if (cell.CustomReorderCommaond == null)
+                        {
+                            if (listView.IsGroupingEnabled)
+                            {
+                                var groups = (IObservableCollectionEx)listView.ItemsSource;
+                                var childrenOfNewGroup = (IObservableCollectionEx)groups[newRowIndexPath.Section];
+                                if (sourceOrNewAppliedIndexPath.Section == newRowIndexPath.Section)
+                                {
+                                    childrenOfNewGroup.Move(sourceOrNewAppliedIndexPath.Row, newRowIndexPath.Row);
+                                }
+                                else
+                                {
+                                    var childrenOfSourceGroup =
+                                        (IObservableCollectionEx)groups[sourceOrNewAppliedIndexPath.Section];
+                                    var childrenToChangeOrder = childrenOfSourceGroup[sourceOrNewAppliedIndexPath.Row];
+                                    tableView.BeginUpdates();
+                                    childrenOfSourceGroup.Remove(childrenToChangeOrder);
+                                    childrenOfNewGroup.Add(newRowIndexPath.Row, childrenToChangeOrder);
+                                    tableView.EndUpdates();
+                                }
+                            }
+                            else
+                            {
+                                var list = (IObservableCollectionEx)listView.ItemsSource;
+                                list.Move(sourceOrNewAppliedIndexPath.Row, newRowIndexPath.Row);
+                            }
+                        }
+                        else
+                        {
+                            cell.CustomReorderCommaond.Execute(new ReorderCommandParam(sourceOrNewAppliedIndexPath.Row, sourceOrNewAppliedIndexPath.Section, newRowIndexPath.Row, newRowIndexPath.Section));
+                        }
+
+                        sourceOrNewAppliedIndexPath = newRowIndexPath;
+                    }
+                    break;
+                case UIGestureRecognizerState.Ended:
+                case UIGestureRecognizerState.Cancelled:
+                case UIGestureRecognizerState.Failed:
+                    if (opaqueView == null)
+                        return;
+
+                    opaqueView.RemoveFromSuperview();
+                    opaqueView = null;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ MovableListView/MovableListView/MovableListView.iOS/MovableViewCellRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using MovableListView;
+using MovableListView.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MovableViewCell), typeof(MovableViewCellRenderer))]
+namespace MovableListView.iOS
+{
+    public class MovableViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var parent = (ListView)item.Parent;
+            var movableViewCell = (MovableViewCell)item;
+            if (movableViewCell.CustomReorderCommaond == null && !(parent.ItemsSource is IObservableCollectionEx))
+                throw new InvalidOperationException("ItemsSource in ListView which contains MovableViewCell must implement IObservableCollectionEx or MovableViewCell.CustomReorderCommaond must be set.");
+
+            var newCell = base.GetCell(item, reusableCell, tv);
+
+            if (newCell.GestureRecognizers == null || !newCell.GestureRecognizers.OfType<MovableCellGestureRecognizer>().Any())
+                newCell.AddGestureRecognizer(MovableCellGestureRecognizer.CreateGesture(parent, tv, movableViewCell, newCell));
+            return newCell;
+        }
+    }
+}
